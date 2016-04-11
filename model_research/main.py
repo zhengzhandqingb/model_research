10e1ed57dcb83cc7386603387cb76e1b883f28a3
zhang zheng <812674168@qq.com>
2016-04-11 16:57:27 +0800

    cc

--- model_research/main.py
+++ model_research/main.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask,request,url_for,render_template
 app = Flask(__name__)
-
 
 @app.route("/test")
 def test():
     return "success"
 
 
+@app.route("/history_model")
+def history_model():
+    model_id = request.form['model_id']
+    date = request.form["date"]
+    #todo 计算模型
+
+
+
+
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(debug=True,host="0.0.0.0",port=5000)
